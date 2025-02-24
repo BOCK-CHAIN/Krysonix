543efee85ef258cc5dcfb9b1b648a7cab9509632
Likith K G <glikith1705@gmail.com>
2025-02-24 17:46:06 +0530

    Delete I-projects directory

--- I-projects/vidchill_tutorial/src/pages/auth/sign-in.tsx
+++
@@ -1,9 +0,0 @@
-import SignIn from "~/Components/SignInForm";
-
-export default function SignInPage() {
-  return (
-    <div className="flex items-center justify-center min-h-screen w-full bg-neutral-900">
-      <SignIn />
-    </div>
-  )
-}
--- I-projects/vidchill_tutorial/src/pages/auth/sign-up.tsx
+++
@@ -1,9 +0,0 @@
-import SignUpForm from "~/Components/SignupForm";
-
-export default function SignupPage() {
-  return (
-    <div className="flex items-center justify-center min-h-screen  w-full bg-neutral-900">
-      <SignUpForm />
-    </div>
-  )
-}
